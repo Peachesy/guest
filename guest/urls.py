cc6072ef383a8abc75231e6ef82039ff5fbf1805
Peachesy <2360950972@qq.com>
2020-05-20 20:02:31 +0800

    some exercise for django

--- guest/urls.py
+++ guest/urls.py
@@ -22,4 +22,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #添加index/路径配置,r表示字符串是非转义的原始字符，^用来匹配文本的开头，$用来匹配文本的结束
     path('index/',views.index),
+    url('login_action/$',views.login_action),
+    url(r'^event_manage/$',views.event_manage),
+    url(r'^$',views.index),
+    url(r'^accounts/login/$',views.index),
 ]
