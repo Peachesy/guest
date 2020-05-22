17c33c9487999fda7caac03bb73cbf846c1de3b5
Peachesy <2360950972@qq.com>
2020-05-22 16:24:56 +0800

    add models，use django's admin management

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from sign.models import Event, Guest
 
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):  # 没生效？？？？？？？？？？？？？？？？？？？？？？
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']  # 增加搜索栏
+    list_filter = ['status']  # 增加过滤器
+
+
+class GuestAdmin(admin.ModelAdmin):     # 没生效？？？？？？？？？？？？？？？？？？？？？？
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+    search_fields = ['realname','phone']  # 搜索栏
+    list_filter = ['sign']  # 过滤器
+
+# 通知admin管理工具为Event和Guest模块提供界面
+admin.site.register(Event)
+admin.site.register(Guest)
+
