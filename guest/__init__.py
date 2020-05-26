538b397c09474dd61297e64b8fc14b94a7497f28
Peachesy <2360950972@qq.com>
2020-05-26 18:33:34 +0800

    delete some conflict

--- guest/__init__.py
+++ guest/__init__.py
@@ -2,7 +2,3 @@ import pymysql
 
 pymysql.version_info=(1,4,6,'final',0)  # 修改myqlclient的版本
 pymysql.install_as_MySQLdb()  # 通过PyMySQL来连接Mysql
-import pymysql
-
-pymysql.version_info=(1,4,6,'final',0)  # 修改myqlclient的版本
-pymysql.install_as_MySQLdb()  # 通过PyMySQL来连接Mysql
